8a0e9fba0d7e4a0d4f968f4c0a7e580c05a01d37
thanakorn <thanakorn.stsb@gmail.com>
2026-02-05 17:20:29 +0700

    feat: enhance server functionality with CORS configuration and posts endpoint

--- app.mjs
+++ app.mjs
@@ -1,17 +1,45 @@
 import 'dotenv/config'
 import express from "express";
 import cors from "cors";
+import connectionPool from './data/db.mjs';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://route-boss-blog-git-dev-boss-projects-a246840e.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello TechUp!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
-});
+app.get("/posts",async (req, res) => {
+  try {
+    const result = await connectionPool.query("SELECT * FROM posts")
+    return res.status(200).json({
+      data : result.rows
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message:"Unable to fetch posts"
+    })
+  }
+})
+
+if (process.env.VERCEL !== "1") {
+  app.listen(port, () => {
+    console.log(`✅ Server running on http://localhost:${port}`);
+  });
+}
+
+export default app;

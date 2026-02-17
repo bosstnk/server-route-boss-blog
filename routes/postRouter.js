51ec9b61d3e752065b834c829c3fe74557eb8573
thanakorn <thanakorn.stsb@gmail.com>
2026-02-17 14:20:22 +0700

    feat(posts): add user authentication to post creation and retrieval

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import postController from "../controllers/postController.js";
 import postCreateValidation from "../middlewares/postCreateValidation.js";
 import postUpdateValidation from "../middlewares/postUpdateValidation.js";
+import { protect } from "../middlewares/protect.js";
 
 const postRouter = Router();
 
@@ -10,6 +11,7 @@ postRouter.get("/", postController.getPosts);
 
 postRouter.post(
     "/",
+    protect,
     postCreateValidation,
     postController.createPost
   );
@@ -18,11 +20,12 @@ postRouter.get("/:postId", postController.getPostById);
 
 postRouter.put(
   "/:postId",
+  protect,
   postUpdateValidation,
   postController.updatePostById
 );
 
-postRouter.delete("/:postId", postController.deletePostById)
+postRouter.delete("/:postId",protect, postController.deletePostById)
 
 
 export default postRouter;
